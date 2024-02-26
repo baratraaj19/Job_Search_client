12d68dbf5f257c4c7d9e22442d8b85bb6829a07e
Baratraaj S <baratraajcr19@gmail.com>
2024-02-26 17:14:18 +0530

    nav bar completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#141414",
+        blue: "#3575e2",
+      },
+    },
   },
   plugins: [],
 };
