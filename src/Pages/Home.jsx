14fa00d9055272ee1b86c34ecf10275d2173cdaf
Baratraaj S <baratraajcr19@gmail.com>
2024-02-26 18:15:26 +0530

    banner finished and started the filter section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Banner from "../components/Banner";
+import { useState } from "react";
 
 const Home = () => {
-  return <div></div>;
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const [query, setQuery] = useState("");
+  const handelInputChange = (e) => {
+    setQuery(e.target.value);
+    // console.log(query);
+  };
+
+  const filteredJobs = jobs.filter(
+    (job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+  console.log(filteredJobs);
+  return (
+    <div>
+      <Banner query={query} handelInputChange={handelInputChange} />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import { FiSearch } from "react-icons/fi";
+import { TfiLocationPin } from "react-icons/tfi";
+
+const Banner = ({ query, handelInputChange }) => {
+  return (
+    <div>
+      <div className=' max-w-screen-2xl container mx-auto xl:px-24 md:px-20 px-4 py-14'>
+        <h1 className='text-5xl font-bold text-primary mb-3'>
+          Find your <span className='text-blue'>DREAM JOB</span> today
+        </h1>
+        <p className='text-lg text-black/70 mb-8'>
+          Lorem ipsum dolor sit amet conseti ducimus dignissimos, aliquid ut ex
+          dolor? Omnis.
+        </p>
+        <form>
+          <div className='flex'>
+            <div className='flex md:rounded-s-md rounded shadow-sm border-2 border-gray-200  ring-1ring-inset  focus-within:ring-2 focus-within:ring-inset focus-within:rounded  focus-within:ring-indigo-600 md:w-1/2 w-full'>
+              <input
+                type='text'
+                name='title'
+                id='title'
+                placeholder='What are you looking for?'
+                onChange={handelInputChange}
+                value={query}
+                className='block flex-1 border-e bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6'
+              />
+              <FiSearch className='absolute mt-2.5 ml-2 text-gray-400' />
+            </div>
+            <div className='flex md:rounded-s-md rounded shadow-sm border-2 border-gray-200  ring-1ring-inset  focus-within:ring-2 focus-within:ring-inset focus-within:rounded  focus-within:ring-indigo-600 md:w-1/2 w-full'>
+              <input
+                type='text'
+                name='title'
+                id='title'
+                placeholder='where are you looking for?'
+                onChange={handelInputChange}
+                value={query}
+                className='block flex-1 border-e bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6'
+              />
+              <TfiLocationPin className='absolute mt-2.5 ml-2 text-gray-400' />
+            </div>
+            <button
+              type='submit'
+              className='py-2 px-8 rounded-md border   bg-blue text-white'>
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
